c385e13e2026156aa6d9c707f79090e2a43cf881
Edgar Alan Balderama <eabalderama@gmail.com>
2023-09-28 02:59:33 +0800

    Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Roboto_Mono } from 'next/font/google';
 import { ReactNode } from 'react';
 import Navbar from './components/Navbar';
+import Script from 'next/script';
 
 const robotoMono = Roboto_Mono({ subsets: ['latin'] });
 
@@ -18,6 +19,16 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-F5H7S8MX7V" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-F5H7S8MX7V');
+        `}
+      </Script>
       <body className={`${robotoMono.className}`}>
         <Navbar />
         <div className="max-w-2xl mx-auto pt-12">{children}</div>
