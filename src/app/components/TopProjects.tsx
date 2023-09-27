174711ed900763d03fba0a0db989c1849e48f53c
Edgar Alan Balderama <eabalderama@gmail.com>
2023-09-28 03:04:33 +0800

    Add project links

--- src/app/components/TopProjects.tsx
+++ src/app/components/TopProjects.tsx
@@ -2,6 +2,7 @@ import infanity from '../assets/images/infanity.png';
 import hyperjump from '../assets/images/hyperjump.png';
 import repit from '../assets/images/repitsocial.png';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function TopProjects() {
   return (
@@ -11,14 +12,16 @@ export default function TopProjects() {
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
         <div>
-          <div className="relative w-full h-56 sm:h-36">
-            <Image
-              src={infanity}
-              fill
-              className="object-cover rounded-lg object-top"
-              alt="Infanity"
-            />
-          </div>
+          <Link href="https://marketplace.infanity.xyz/" target="_blank">
+            <div className="relative w-full h-56 sm:h-36">
+              <Image
+                src={infanity}
+                fill
+                className="object-cover rounded-lg object-top"
+                alt="Infanity"
+              />
+            </div>
+          </Link>
           <h1 className="text-lg font-bold">Infanity</h1>
           <p className="text-xs text-justify">
             I was responsible in the development of the whole project excluding
@@ -28,14 +31,16 @@ export default function TopProjects() {
           </p>
         </div>
         <div>
-          <div className="relative w-full h-56 sm:h-36">
-            <Image
-              src={hyperjump}
-              fill
-              className="object-cover rounded-lg object-top"
-              alt="HyperJump"
-            />
-          </div>
+          <Link href="https://bsc.hyperjump.app/" target="_blank">
+            <div className="relative w-full h-56 sm:h-36">
+              <Image
+                src={hyperjump}
+                fill
+                className="object-cover rounded-lg object-top"
+                alt="HyperJump"
+              />
+            </div>
+          </Link>
           <h1 className="text-lg font-bold">HyperJump</h1>
           <p className="text-xs text-justify">
             I was tasked to add features to the existing DE-FI app. The features
@@ -44,14 +49,16 @@ export default function TopProjects() {
           </p>
         </div>
         <div>
-          <div className="relative w-full h-56 sm:h-36">
-            <Image
-              src={repit}
-              fill
-              className="object-cover rounded-lg object-top"
-              alt="RepIt Social"
-            />
-          </div>
+          <Link href="https://repitsocial.com/" target="_blank">
+            <div className="relative w-full h-56 sm:h-36">
+              <Image
+                src={repit}
+                fill
+                className="object-cover rounded-lg object-top"
+                alt="RepIt Social"
+              />
+            </div>
+          </Link>
           <h1 className="text-lg font-bold">RepIt Social</h1>
           <p className="text-xs text-justify">
             One of the pioneer developers of RepIt Social. I was one of the
